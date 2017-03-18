d1e02e6ebbf5ecda166db4bdc7314b4bad758585
dbetts <dbetts@ethz.ch>
2017-03-18 10:57:55 +0100

    added an edge detection test for camera

--- src/python/vision/test_camera.py
+++ src/python/vision/test_camera.py
@@ -1,9 +1,13 @@
 import time
 import picamera
 import numpy as np
+from skimage import data
+from skimage.exposure import rescale_intensity
+import matplotlib.pyplot as plt
+
 
 with picamera.PiCamera() as camera:
-    #2592×1944
+    #2592ï¿½1944
     camera.resolution = (2592, 1944)
     camera.framerate = 24
     time.sleep(2)
@@ -11,3 +15,21 @@ with picamera.PiCamera() as camera:
     camera.capture(output, 'rgb')
     output = output.reshape((2592, 1944, 3))
     #output = output[:100, :100, :]
+
+#image = data.astronaut()
+
+fig = plt.figure(figsize=(14, 7))
+ax_each = fig.add_subplot(121, adjustable='box-forced')
+ax_hsv = fig.add_subplot(122, sharex=ax_each, sharey=ax_each,
+                         adjustable='box-forced')
+
+# We use 1 - sobel_each(image)
+# but this will not work if image is not normalized
+ax_each.imshow(rescale_intensity(1 - sobel_each(output)))
+ax_each.set_xticks([]), ax_each.set_yticks([])
+ax_each.set_title("Sobel filter computed\n on individual RGB channels")
+
+# We use 1 - sobel_hsv(image) but this will not work if image is not normalized
+ax_hsv.imshow(rescale_intensity(1 - sobel_hsv(output)))
+ax_hsv.set_xticks([]), ax_hsv.set_yticks([])
+ax_hsv.set_title("Sobel filter computed\n on (V)alue converted image (HSV)")
