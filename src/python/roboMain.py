2e4aea098c6dad6d160d2e8d5620da10f3ff7e39
Duncan Betts <dbetts@ethz.ch>
2018-04-02 20:19:46 +0200

    yab

--- src/python/roboMain.py
+++ src/python/roboMain.py
@@ -176,7 +176,7 @@ class Robot(object):
                 self.distance = 120 
             self.distance*= self.ureg.cm
             temp = np.zeros(3)* self.ureg.cm
-            temp[0]=(self.distance+self.bodyRadius
+            temp[0]=(self.distance+self.bodyRadius)
             self.observations.append(self.rotation.dot(temp))
             #print(self.distance)
             # set the motor speed based upon the PID
