6131a0f1f83695c69a27d14c546aab4d3c61e944
Duncan Betts <dbetts@ethz.ch>
2018-04-07 18:36:35 +0200

    pint difference between py2 and py3

--- src/python/mapping.py
+++ src/python/mapping.py
@@ -19,9 +19,7 @@ class observation:
         return opp
     
     def global_OPP(self, coarse=5*ureg.cm):
-        print(self.distance)
-        print(coarse)
-        rect = int((round(self.distance + 2*2.5*ureg.cm)/coarse + 1).magnitude)
+        rect = int(round(((self.distance + 2*2.5*ureg.cm)/coarse + 1).magnitude))
         width = int((tan((10*ureg.degrees).to('radians'))*rect))+1
         X, Y = np.meshgrid(range(1,rect), range(-width,width+1))*coarse
         theta = np.arctan(Y/X)*ureg.radians
